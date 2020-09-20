5855eb3ca932d80424f8f17515c4326a36532747
Maxim Grabarnik <maxim@lightricks.com>
2020-09-20 18:16:00 +0100

    multi-stage start response.

--- main.py
+++ main.py
@@ -25,18 +25,39 @@ dispatcher = updater.dispatcher
 #                       url_path=TOKEN)
 
 # updater.bot.set_webhook(HEROKU_URL + "/" + TOKEN)
+chat_id_to_attempt_count = {}
 
 # 
 def start(bot, update):
     chat_id = update.message.chat.id
+    attempt_count = chat_id_to_attempt_count.get(chat_id, 0)
 
-    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-    time.sleep(2)
-    update.message.reply_text("Ну чаво ???")
-    time.sleep(1)
-    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-    time.sleep(2)
-    update.message.reply_text("Я только родился и ничего не умею.")
+    if attempt_count == 0:
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+        time.sleep(2)
+        update.message.reply_text("Ну чаво ???")
+        time.sleep(1)
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+        time.sleep(2)
+        update.message.reply_text("Я только родился и ничего не умею.")
+    elif attempt_count == 1:
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+        time.sleep(2)
+        update.message.reply_text("Я что, на тормоза попал?")
+        time.sleep(1)
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+        time.sleep(2)
+        update.message.reply_text("Н О В Ы Й   Я.")
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+        time.sleep(2)
+        update.message.reply_text("Н И Ч Е Г О   Н Е   У М Е Ю ! ! !")
+    else:
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+        time.sleep(1)
+        update.message.reply_text("отстань")
+        
+
+    chat_id_to_attempt_count[chat_id] = attempt_count + 1
 
     # title = update.message.text
     # title = title.replace("/start@"+BOT_NAME, "").strip()
